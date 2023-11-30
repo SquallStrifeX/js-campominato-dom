199c679ab82eab92883108ea26fd16fb1e32b04d
SquallStrifeX <squallstrifex@gmail.com>
2023-11-30 19:48:48 +0100

    end exercise

--- Base/js/script.js
+++ Base/js/script.js
@@ -1,51 +1,59 @@
 
+function numberCasualGenerate (){
+  return Math.floor(Math.random() * 100) + 1
+}
+
+function bombGenerator (){
+j=1;
+while (j<16){
+  let numeroCasuale = numberCasualGenerate();
+  if(!ArrayCasuale.includes(numeroCasuale)){
+    ArrayCasuale.push(numeroCasuale);
+    j++;
+  }
+}}
+
+
+let ArrayCasuale = [""]
+
+bombGenerator()
+console.log(ArrayCasuale)
+
+
 let buttonStart = document.getElementById('button_start');
 
+ let containerPadre = document.getElementById('container_padre');  
+
+
 buttonStart.addEventListener("click", function(){
- containerPadre.innerHTML = " "
- let input_difficulty = document.getElementById('difficolta');
- let difficulty = parseInt(input_difficulty.value);
- console.log(difficulty)
-
-
-    switch (difficulty){
-      case 1:
-        containerPadre.innerHTML = " "
-        for(i=0; i<100; i++){
-          creazioneDiUnQuadrato();
-            }
-            break
-      case 2:
-        containerPadre.innerHTML = " "
-        for(i=0; i<81; i++){
-          creazioneDiUnQuadrato();
-            }
-            containerPadre.classList.add("container_81")
-
-            break
-      case 3:
-         containerPadre.innerHTML = " "
-         for(i=0; i<49; i++){
-           creazioneDiUnQuadrato();
-            }
-            containerPadre.classList.add("container_49")
-             break     
-
-    }
-    
-   
-})
-
-let containerPadre = document.getElementById('container_padre')
-
-  function creazioneDiUnQuadrato () {
-    let nuovoQuadrato = document.createElement("div");
+  containerPadre.innerHTML = " "
+    for(i=1; i<100; i++){
+    nuovoQuadrato = document.createElement("div")
     nuovoQuadrato.classList.add("casella");
     containerPadre.appendChild(nuovoQuadrato);
-    nuovoQuadrato.textContent = (i+1);
-    nuovoQuadrato.addEventListener("click", function onClick(event){
-    nuovoQuadrato.classList.toggle("bg-warning");
-    console.log(nuovoQuadrato.textContent)
+    nuovoQuadrato.textContent = (i);
+    console.log(nuovoQuadrato)
 
+    nuovoQuadrato.addEventListener("click", function(){
+      if(!ArrayCasuale.includes(nuovoQuadrato.textContent)){
+        nuovoQuadrato.classList.add("bg-primary")
+      }
+      else{
+        nuovoQuadrato.classList.add("bg-danger")
+      }
+      
     })
-}
+    
+
+}})
+
+
+ 
+  
+
+{
+    
+    
+  }
+
+  
--- Base/readme.md
+++ Base/readme.md
@@ -7,6 +7,15 @@ Attenzione: **nella stessa cella può essere posizionata al massimo una bomba, p
 In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
 La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
 Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
+1- Generiamo 16 numeri casuali in base al livello di difficoltà
+2- mettiamo questi 16 numeri in un array e facciamo in modo che non ci siano duplicati
+3- facciamo in modo che cliccando sulla cella, la cella diventi rossa se trova un numero dell'array oppure di azzurra se non
+4- facciamo terminare la partita se il giocatore trova una bomba
+5- mostriamo a schermo il punteggio totale ottenuto
+
+
+
 BONUS:
 Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
 difficoltà 1 ⇒ 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
