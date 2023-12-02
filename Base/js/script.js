5bca488afe9a9ceec21d3e2ffd69d5c3d75a6638
SquallStrifeX <squallstrifex@gmail.com>
2023-12-02 17:49:45 +0100

    end exercise

--- Base/js/script.js
+++ Base/js/script.js
@@ -23,23 +23,40 @@ console.log(ArrayCasuale)
 let buttonStart = document.getElementById('button_start');
 
  let containerPadre = document.getElementById('container_padre');  
+ let point = 0
+ let string_point = document.getElementById('punteggio');
+
+
+ string_point.innerHTML = "Il tuo punteggio è di " + point + " Punti"
 
 
 buttonStart.addEventListener("click", function(){
   containerPadre.innerHTML = " "
     for(i=1; i<100; i++){
-    nuovoQuadrato = document.createElement("div")
+    let nuovoQuadrato = document.createElement("button")
     nuovoQuadrato.classList.add("casella");
     containerPadre.appendChild(nuovoQuadrato);
     nuovoQuadrato.textContent = (i);
     console.log(nuovoQuadrato)
 
-     
+    nuovoQuadrato.addEventListener("click", function(){
+      let numberclick = parseInt(nuovoQuadrato.textContent)
+      console.log(numberclick)
+      if(ArrayCasuale.includes(numberclick)){
+      nuovoQuadrato.classList.add("bg-danger");
+      string_point.innerHTML = "Hai perso! Il tuo punteggio totale è di " + point + " Punti";
+
+      }else{
+        nuovoQuadrato.classList.add("bg-primary")
+        point++
+        string_point.innerHTML = "Il tuo punteggio è di " + point + " Punti"
+      }
+      })
+      
+
    
       
     }})
 
-    nuovoQuadrato.addEventListener("click", function(){
-      nuovoQuadrato.classList.add("bg-primary")
-      })
+  
   
